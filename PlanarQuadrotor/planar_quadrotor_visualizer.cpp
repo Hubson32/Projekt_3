28c4906798a845e9fbc9907502f5e2f6e2054312
Jakub Bulski <127137737+Bulonn@users.noreply.github.com>
2023-05-22 20:13:02 +0200

    Update planar_quadrotor_visualizer.cpp
    Przejscie na tekstury i wychylanie z rownowagi w zlaeznosci od thety

--- PlanarQuadrotor/planar_quadrotor_visualizer.cpp
+++ PlanarQuadrotor/planar_quadrotor_visualizer.cpp
@@ -33,36 +33,48 @@ void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer> &gRenderer)
 
     // "Kadlub" drona
     SDL_Rect body = {q_x_screen - 85, q_y_screen - 30, 170, 9};
-    SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xA5, 0x2A, 0x2A); 
-    SDL_RenderFillRect(gRenderer.get(), &body);
     //Pierwsze smiglo
     SDL_Rect prop1 = { q_x_screen - 95, q_y_screen - 51 , 65, 5 };
     //Drugie smiglo
     SDL_Rect prop2 = { q_x_screen + 35, q_y_screen - 51 , 65, 5 };
     
-    SDL_Surface* buffer = SDL_LoadBMP("prop.bmp");
-	if ( !buffer ) {
+    
+    SDL_Surface* buffer1 = SDL_LoadBMP("body.bmp");
+	if ( !buffer1 ) {
+		std::cout << "Error loading image body.bmp: " << SDL_GetError() << std::endl;
+    }
+
+    SDL_Texture* body_texture;
+	body_texture = SDL_CreateTextureFromSurface(gRenderer.get(),buffer1);
+	SDL_FreeSurface(buffer1);
+	buffer1 = NULL;
+	if ( !body_texture ) {
+		std::cout << "Error creating texture: " << SDL_GetError() << std::endl;
+	}
+     
+    SDL_Surface* buffer2 = SDL_LoadBMP("prop.bmp");
+	if ( !buffer2 ) {
 		std::cout << "Error loading image prop.bmp: " << SDL_GetError() << std::endl;
     }
 
+    SDL_RenderCopyEx(gRenderer.get(), body_texture, NULL, &body, q_theta * 0.2 * (180.0/M_PI), NULL, SDL_FLIP_NONE);
+
     SDL_Texture* prop_texture;
-	prop_texture = SDL_CreateTextureFromSurface(gRenderer.get(),buffer);
-	SDL_FreeSurface(buffer);
-	buffer = NULL;
+	prop_texture = SDL_CreateTextureFromSurface(gRenderer.get(),buffer2);
+	SDL_FreeSurface(buffer2);
+	buffer2 = NULL;
 	if ( !prop_texture ) {
 		std::cout << "Error creating texture: " << SDL_GetError() << std::endl;
 	}
     SDL_RenderCopyEx(gRenderer.get(), prop_texture, nullptr, &prop1, angle, NULL, SDL_FLIP_NONE);
     SDL_RenderCopyEx(gRenderer.get(), prop_texture, NULL, &prop2, angle, NULL, SDL_FLIP_NONE);
-     
+
      //Podporki
     SDL_Rect cant1 = {q_x_screen - 65, q_y_screen - 46 , 4, 17};
-    SDL_SetRenderDrawColor(gRenderer.get(), 47, 61, 93, 30); 
-    SDL_RenderFillRect(gRenderer.get(), &cant1);
-
     SDL_Rect cant2 = {q_x_screen + 65, q_y_screen - 46 , 4, 17};
-    SDL_SetRenderDrawColor(gRenderer.get(), 47, 61, 93, 30); 
-    SDL_RenderFillRect(gRenderer.get(), &cant2);
+    SDL_RenderCopyEx(gRenderer.get(), prop_texture, NULL, &cant1, q_theta * 0.4 * (180.0/M_PI), NULL, SDL_FLIP_NONE);
+    SDL_RenderCopyEx(gRenderer.get(), prop_texture, NULL, &cant2, q_theta * 0.4 * (180.0/M_PI), NULL, SDL_FLIP_NONE);
+   
 
     SDL_RenderPresent(gRenderer.get());
     
